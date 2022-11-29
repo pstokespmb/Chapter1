99fb01a9fd6e66c725a42480c1b8c9c05f10ce5f
pstokespmb <peter_stokes@berkeley.edu>
2022-11-29 14:18:13 -0800

    commit

---
+++ 03_jointGenotyping/Chapter1_Chr02_L36.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/users/peter_stokes/VariantCalling/newVC/VCF
+#SBATCH -J GATK_geno_Chr02_L36
+#SBATCH --partition=savio
+#SBATCH --account=co_rosalind
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=6:00:00
+#SBATCH --mem=64000
+#SBATCH -o /global/scratch/users/peter_stokes/err_outs/GATK_geno_Chr02_L36.out
+#SBATCH -e /global/scratch/users/peter_stokes/err_outs/GATK_geno_Chr02_L36.err
+#SBATCH --mail-user=peter_stokes@berkeley.edu
+#SBATCH --mail-type=All
+
+module load java
+
+/global/home/users/peter_stokes/software/gatk-4.3.0.0/gatk \
+--java-options \
+"-Djava.io.tmpdir=/global/scratch/users/peter_stokes/temp_files/ -Xmx50g" \
+GenotypeGVCFs \
+-R /global/scratch/users/peter_stokes/references/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V gendb:///global/scratch/users/peter_stokes/VariantCalling/newVC/DBI/DBI_Chr02 \
+-O /global/scratch/users/peter_stokes/VariantCalling/newVC/VCF/PS_Chapter1_Chr02_L36.vcf.gz \
+-L  HanXRQChr02:52500001-54000000 \
+--max-alternate-alleles 6 \
+--genomicsdb-max-alternate-alleles 9 \
+--include-non-variant-sites
---
+++ 03_jointGenotyping/Chapter1_Chr08_L105.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/users/peter_stokes/VariantCalling/newVC/VCF
+#SBATCH -J GATK_geno_Chr08_L105
+#SBATCH --partition=savio
+#SBATCH --account=ac_acblackman
+#SBATCH --qos=savio_normal
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=6:00:00
+#SBATCH --mem=64000
+#SBATCH -o /global/scratch/users/peter_stokes/err_outs/GATK_geno_Chr08_L105.out
+#SBATCH -e /global/scratch/users/peter_stokes/err_outs/GATK_geno_Chr08_L105.err
+#SBATCH --mail-user=peter_stokes@berkeley.edu
+#SBATCH --mail-type=All
+
+module load java
+
+/global/home/users/peter_stokes/software/gatk-4.3.0.0/gatk \
+--java-options \
+"-Djava.io.tmpdir=/global/scratch/users/peter_stokes/temp_files/ -Xmx50g" \
+GenotypeGVCFs \
+-R /global/scratch/users/peter_stokes/references/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V gendb:///global/scratch/users/peter_stokes/VariantCalling/newVC/DBI/DBI_Chr08 \
+-O /global/scratch/users/peter_stokes/VariantCalling/newVC/VCF/PS_Chapter1_Chr08_L105.vcf.gz \
+-L  HanXRQChr08:156000001-157500000 \
+--max-alternate-alleles 6 \
+--genomicsdb-max-alternate-alleles 9 \
+--include-non-variant-sites
